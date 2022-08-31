88cf5781e0e133ed9e9eb1c4cc1f841ec9c18a09
kayyenl <yenyenlee9@gmail.com>
2022-08-31 17:12:12 +0800

    attempting the first medium actually

---
+++ easies.jsx
@@ -0,0 +1,14 @@
+// 1. Container with Most Water (easy)
+
+// You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+// Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+// Return the maximum amount of water a container can store
+
+// Input: height = [1,1]
+// Output: 1
+
+//strategy: find the highest and second highest heights in the array
+// but then there could be long and short ones?
+// no
