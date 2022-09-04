0105218d073ca49e2362588277087ebc257aa7b0
kayyenl <yenyenlee9@gmail.com>
2022-09-05 00:00:16 +0800

    going to start on easy 5

--- easies.js
+++ easies.js
@@ -175,7 +175,7 @@ function BuySellOptimal(prices) {
 // An integer is a palindrome when it reads the same backward as forward.
 
 // For example, 121 is a palindrome while 123 is not.
- 
+
 
 // Example 1:
 // Input: x = 121
@@ -203,6 +203,23 @@ function isPalindrome(x) {
     } return true
 }
 
-console.log(isPalindrome(121))
-console.log(isPalindrome(-121))
-console.log(isPalindrome(10))
+// console.log(isPalindrome(121))
+// console.log(isPalindrome(-121))
+// console.log(isPalindrome(10))
+
+
+// -------------------------------------------------------------------
+
+// 5. Longest Common Prefix
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+// Example 1:
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+
+// Example 2:
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
