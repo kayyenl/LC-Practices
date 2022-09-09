a0b19afe446c94877f8775f3c53b6760daa04deb
kayyenl <yenyenlee9@gmail.com>
2022-09-09 13:53:11 +0900

    stuck at the easy 7, my first linkedlist qn

--- easies.js
+++ easies.js
@@ -302,5 +302,43 @@ function LargestContInSubs(nums) {
     return maxRes
 }
 
-console.log(LargestContInSubs([1,3,5,4,7]))
-console.log(LargestContInSubs([2,2,2,2,2]))
+// console.log(LargestContInSubs([1,3,5,4,7]))
+// console.log(LargestContInSubs([2,2,2,2,2]))
+// ------------------------------------------------------------------
+// 7. Merge Two Sorted Lists
+
+// You are given the heads of two sorted linked lists list1 and list2.
+
+// Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+// Return the head of the merged linked list.
+// Example 1:
+// Input: list1 = [1,2,4], list2 = [1,3,4]
+// Output: [1,1,2,3,4,4]
+
+// Example 2:
+// Input: list1 = [], list2 = []
+// Output: []
+
+// Example 3:
+// Input: list1 = [], list2 = [0]
+// Output: [0]
+
+function MergeTwoSortedLists(list1, list2) {
+    const list3 = []
+    let var1, var2
+    let index1 = 0
+    let index2 = 0
+    while (list1.length + list2.length > 0) {
+        var1 = list1[index1]
+        var2 = list2[index2]
+        if (var1 !== undefined && var2 !== undefined) {
+            list3.push(Math.max(var1, var2))
+            list3.push(Math.min(var1, var2))
+        }
+        if (var1 === undefined) {}
+        if (var2 === undefined) {}
+    } return list3
+}
+
+console.log(MergeTwoSortedLists([1,2,4],[1,3,4]))
