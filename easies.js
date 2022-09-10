12c656ec3d1b750bab29622660052b39d4f111ae
kayyenl <yenyenlee9@gmail.com>
2022-09-10 12:50:33 +0900

    must remember to use .val for comparisons and also point to the head!

--- easies.js
+++ easies.js
@@ -380,4 +380,23 @@ function Merge2SortedLists2(list1, list2) {
     }
 }
 
-function 
+function MergeTwoSortedIm(list1, list2) {
+    let curr = new ListNode()
+    const dummy = curr
+    while (list1&&list2) {
+        if (list1.val < list2.val) {
+            curr.next = list1
+            list1 = list1.next
+        } else {
+            curr.next = list2
+            list2 = list2.next
+        } curr = curr.next
+    } 
+    if (list1 === null) {
+        curr.next = list2
+    } 
+    if (list2 === null) {
+        curr.next = list1
+    } 
+    return dummy.next
+}
