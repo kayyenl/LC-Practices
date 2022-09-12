1d1b2a2c719277ac4772d96d8f8318042e4ae3d5
kayyenl <yenyenlee9@gmail.com>
2022-09-12 16:00:26 +0900

    finally solved this add binary question whew had to be efficient

--- easies.js
+++ easies.js
@@ -688,11 +688,13 @@ function addBinarySimple(a, b) {
         returnstr = digit + returnstr //this is prepended
         remainder = sum > 1 ? 1 : 0
         i--, j--
-    } returnstr = (remainder ? "1" : "0") + returnstr
+    } 
+    returnstr = (remainder ? "1" : "") + returnstr
     return returnstr
 }
 
 console.log(addBinarySimple("11", "1"))
+console.log(addBinarySimple("0", "0"))
 console.log(addBinarySimple("1010", "1011"))
 console.log(addBinarySimple("10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101",
 "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"))
