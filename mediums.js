74e75b81bfafc7fff4a77259e64e1efe4f92649c
kayyenl <yenyenlee9@gmail.com>
2022-09-13 20:30:48 +0900

    trying to create an object, doing medium 8 right now

--- mediums.js
+++ mediums.js
@@ -450,6 +450,71 @@ function MinRotSort(nums) {
     } return nums[l]
 }
 
-console.log(MinRotSort([3,4,5,1,2]))
-console.log(MinRotSort([4,5,6,7,0,1,2]))
-console.log(MinRotSort([11,13,15,17]))
+// console.log(MinRotSort([3,4,5,1,2]))
+// console.log(MinRotSort([4,5,6,7,0,1,2]))
+// console.log(MinRotSort([11,13,15,17]))
+
+// -------------------------------------------------------------------
+// 8. Min Stack
+// Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+// Implement the MinStack class:
+
+// MinStack() initializes the stack object.
+// void push(int val) pushes the element val onto the stack.
+// void pop() removes the element on the top of the stack.
+// int top() gets the top element of the stack.
+// int getMin() retrieves the minimum element in the stack.
+// You must implement a solution with O(1) time complexity for each function.
+
+// Example 1:
+// Input
+// ["MinStack","push","push","push","getMin","pop","top","getMin"]
+// [[],[-2],[0],[-3],[],[],[],[]]
+
+// Output
+// [null,null,null,null,-3,null,0,-2]
+
+// Explanation
+// MinStack minStack = new MinStack();
+// minStack.push(-2);
+// minStack.push(0);
+// minStack.push(-3);
+// minStack.getMin(); // return -3
+// minStack.pop();
+// minStack.top();    // return 0
+// minStack.getMin(); // return -2
+
+var MinStack = function() {
+    this.array = []
+    this.lowestval
+}
+
+MinStack.prototype.push = function(val) {
+    this.array.unshift(val)
+};
+
+MinStack.prototype.pop = function() {
+    this.array.shift()
+};
+
+MinStack.prototype.top = function() {
+    var returnval = this.array.shift()
+    this.array.unshift(returnval)
+    return returnval
+};
+
+MinStack.prototype.getMin = function() {
+    return this.lowestval
+};
+
+let min = new MinStack()
+min.push(-2)
+min.push(0)
+min.push(-3)
+min.getMin()
+min.pop()
+min.top()
+min.getMin()
+
+console.log(min.array)
