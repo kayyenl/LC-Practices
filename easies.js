da937ef63759312294f83fc908dc069d60133876
kayyenl <yenyenlee9@gmail.com>
2022-09-14 01:36:24 +0900

    solved easy 13 (roman to numeral) in 27 minutes!!!

--- easies.js
+++ easies.js
@@ -698,3 +698,69 @@ function addBinarySimple(a, b) {
 // console.log(addBinarySimple("1010", "1011"))
 // console.log(addBinarySimple("10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101",
 // "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"))
+
+// ------------------------------------------------------------------
+// 13. Roman to SVGAnimatedIntegerRoman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+// For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+// Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+// I can be placed before V (5) and X (10) to make 4 and 9. 
+// X can be placed before L (50) and C (100) to make 40 and 90. 
+// C can be placed before D (500) and M (1000) to make 400 and 900.
+// Given a roman numeral, convert it to an integer.
+
+ 
+
+// Example 1:
+// Input: s = "III"
+// Output: 3
+// Explanation: III = 3.
+
+// Example 2:
+// Input: s = "LVIII"
+// Output: 58
+// Explanation: L = 50, V= 5, III = 3.
+
+// Example 3:
+// Input: s = "MCMXCIV"
+// Output: 1994
+// Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+let hashMap = {}
+hashMap["I"] = 1
+hashMap["V"] = 5
+hashMap["X"] = 10
+hashMap["L"] = 50
+hashMap["C"] = 100
+hashMap["D"] = 500
+hashMap["M"] = 1000
+
+console.log(hashMap["I"])
+function RomanToNum(s) {
+    let returnval = 0
+    let valbefore = 0
+    let valnow = 0
+
+    for (let i = 0; i < s.length; i++) {
+        valnow = hashMap[s[i]]
+        returnval += valnow
+        if (valnow > valbefore && i !== 0) {
+            returnval -= 2*(valbefore)
+        }
+        valbefore = valnow
+    } return returnval
+}
+
+console.log(RomanToNum("III"))
+console.log(RomanToNum("LVIII"))
+console.log(RomanToNum("MCMXCIV"))
