1e0f8d70db133307544b934aec83a2b9f3ac4d0c
kayyenl <yenyenlee9@gmail.com>
2022-09-16 09:26:25 +0900

    attempting the valid parentheses question, harder than it looks to me

--- easies.js
+++ easies.js
@@ -764,3 +764,38 @@ function RomanToNum(s) {
 // console.log(RomanToNum("III"))
 // console.log(RomanToNum("LVIII"))
 // console.log(RomanToNum("MCMXCIV"))
+
+// ------------------------------------------------------------------
+// 14. Valid Parentheses
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+ 
+// Example 1:
+// Input: s = "()"
+// Output: true
+
+// Example 2:
+// Input: s = "()[]{}"
+// Output: true
+
+// Example 3:
+// Input: s = "(]"
+// Output: false
+
+//Preprocessing
+const bracketMap = {}
+bracketMap["("] = ")"
+bracketMap["["] = "]"
+bracketMap["{"] = "}"
+
+function ValidParentheses(s) {
+    let closingCheck
+    for (let i = 0; i < s.length; i++) {
+        if (i === 0 && bracketMap.has(s[i])) 
+    }
+}
