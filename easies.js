72493dbdbae2437d8360c83ce8059e7315a8c9bf
kayyenl <yenyenlee9@gmail.com>
2022-09-20 21:53:00 +0900

    still stuck on easy 16, maybe im getting rusty

--- easies.js
+++ easies.js
@@ -924,3 +924,16 @@ function removeDupesDebugged(nums) {
 // 1. 1 step + 1 step + 1 step
 // 2. 1 step + 2 steps
 // 3. 2 steps + 1 step
+
+function climbStair(n, memo = {}) {
+    if (n in memo) return memo[n]
+    if (n === 1) {
+        memo[n] = 1
+        return 1
+    } 
+    if (n === 2) {
+        memo[n] = 2
+        return 2
+    }
+    memo[n] = 
+}
