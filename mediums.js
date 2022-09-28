2802f3bcb4812163ce3e8a243835cb7c5a2773d8
kayyenl <yenyenlee9@gmail.com>
2022-09-28 22:06:56 +0900

    im going to do an easy tomorrow i think

--- mediums.js
+++ mediums.js
@@ -711,7 +711,7 @@ function coinChange2(coins, amount, memo = {}) {
 // Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
 // Total amount you can rob = 1 + 3 = 4.
 
-// Example 2:
+// Example 2: 
 // Input: nums = [2,7,9,3,1]
 // Output: 12
 // Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
